58a019c66c4ee94d584d6797ae5c0f83a9ec35a6
Henrik Pejer <henrik@uppdragshuset.se>
2014-02-26 10:22:14 +0100

    Added a string class just for fun

---
+++ Tests/utils/StringTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *
+ * Created by: Henrik Pejer, mr@henrikpejer.com
+ * Date: 2014-02-21 13:49
+ *
+ */
+
+namespace utils;
+
+
+use DHP\components\utils\String;
+
+class StringTest extends \PHPUnit_Framework_TestCase {
+
+    /** @var  \DHP\components\utils\String */
+    private $object;
+
+    public function setUp(){
+        $this->object = new String('testValue');
+    }
+
+    public function testValue(){
+        $this->assertEquals('testValue',(string)$this->object);
+        $o = $this->object;
+        $o('newValue');
+        $this->assertEquals('newValue',(string)$this->object);
+
+        $this->assertEquals('testValue', (string)$this->object->replace('new','test'));
+        $this->assertEquals('  testValue',(string)$this->object->pad(11,' ', STR_PAD_LEFT));
+        $this->assertEquals('  testtest',(string)$this->object->pregReplace('#Value#','test'));
+        $matches = array();
+        $this->assertTrue($this->object->pregMatch('#(estt)#',$matches));
+        $this->assertEquals(array(), $matches);
+    }
+}
---
+++ lib/DHP/components/utils/String.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace DHP\components\utils;
+
+/**
+ *
+ * Created by: Henrik Pejer, mr@henrikpejer.com
+ * Date: 2014-02-21 13:44
+ *
+ */
+
+class String
+{
+
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * Mimics str_replace behaviour
+     *
+     * @param string|array $search  what to search for
+     * @param string|array $replace what to replace with
+     * @param null|int     $count   how many times to replace
+     *
+     * @return $this
+     */
+    public function replace($search, $replace, $count = null)
+    {
+        $this->value = str_replace($search, $replace, $this->value, $count);
+        return $this;
+    }
+
+    /**
+     * Mimics str_pad behaviour
+     *
+     * @param int    $length    how long should the string be padded to
+     * @param string $padString the string to pad with
+     * @param int    $padType   STR_PAD_x - constant values
+     *
+     * @return $this
+     */
+    public function pad($length, $padString = null, $padType = null)
+    {
+        $this->value = str_pad($this->value, $length, $padString, $padType);
+        return $this;
+    }
+
+    /**
+     * @param string $pattern     pattern to match
+     * @param string $replacement what to replace pattern with
+     * @param int    $limit       how many times should we replace
+     * @param null   $count       count variable
+     *
+     * @return $this
+     */
+    public function pregReplace($pattern, $replacement, $limit = -1, &$count = null)
+    {
+        $this->value = preg_replace($pattern, $replacement, $this->value, $limit, $count);
+        return $this;
+    }
+
+    /**
+     * Mimics preg_match behaviour
+     *
+     * @param string     $pattern Pattern to match
+     * @param array|null $matches found matches
+     * @param int        $flags
+     * @param int        $offset
+     *
+     * @return $this
+     */
+    public function pregMatch($pattern, &$matches = null, $flags = 0, $offset = 0)
+    {
+        return preg_match($pattern, $this->value, $matches, $flags, $offset);
+    }
+
+    /**
+     * Mimics preg_match_all behaviour
+     *
+     * @param      $pattern
+     * @param null $matches
+     * @param int  $flags
+     * @param int  $offset
+     *
+     * @return $this
+     */
+    public function pregMatchAll($pattern, &$matches = null, $flags = 0, $offset = 0)
+    {
+        return preg_match_all($pattern, $this->value, $matches, $flags, $offset);
+    }
+
+    /**
+     * Used to update the value of the string
+     */
+    public function __invoke()
+    {
+        $this->value = func_get_arg(0);
+    }
+
+    /**
+     * When class is used in an echo-type context, we return the value
+     *
+     * @return mixed
+     */
+    public function __toString()
+    {
+        return $this->value;
+    }
+}
